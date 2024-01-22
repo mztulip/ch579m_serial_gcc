e431461badcab1a3129445a09261b8f6c8439a4b
Mateusz <mateusz@tulip.lol>
2024-01-22 18:38:09 +0100

    objcopy path added

--- make.sh
+++ make.sh
@@ -5,6 +5,7 @@ TOOLCHAIN="/home/tulip/gcc-arm-none-eabi-10.3-2021.10-x86_64-linux/gcc-arm-none-
 TOOLCHAIN_GCC=$TOOLCHAIN"gcc"
 TOOLCHAIN_SIZE=$TOOLCHAIN"size"
 TOOLCHAIN_OBJDUMP=$TOOLCHAIN"objdump"
+TOOLCHAIN_OBJCOPY=$TOOLCHAIN"objcopy"
 echo $TOOLCHAIN_GCC
 
 # -c => Compile or assemble the source files, but do not link. 
