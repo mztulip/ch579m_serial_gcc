908d0e766bc0c297c13bdfb7c4e630b84d2814df
Mateusz <mateusz@tulip.lol>
2024-01-24 21:45:34 +0100

    newlib calls basic implementation added

--- Main.c
+++ Main.c
@@ -2,6 +2,8 @@
 //mateusz@tulip.lol
 
 #include "CH57x_common.h"
+#include <stdio.h>
+
 
 void write_serial(char *str)
 {
@@ -28,6 +30,7 @@ int main()
   UART1_BaudRateCfg(9600);
 
   write_serial("\n\rHello");
+  printf("\n\rHello from printf");
 
 	while(1)
 	{
@@ -51,3 +54,88 @@ void Default_Handler(void)
 		GPIOB_ResetBits( GPIO_Pin_0 );
 	}
 }
+
+
+// https://www.embecosm.com/appnotes/ean9/ean9-howto-newlib-1.0.html#id2719973
+int
+_write (int   file,
+        char *buf,
+        int   nbytes)
+{
+  int i;
+
+  /* Output character at at time */
+  for (i = 0; i < nbytes; i++)
+    {
+      UART1_SendString (&buf[i], 1);
+    }
+        
+  return nbytes;
+
+} 
+
+void *
+_sbrk (int nbytes)
+{
+  static uint8_t heap[512];
+  static uint16_t last = 0;
+  if(last+nbytes < 512)
+  {
+    void* pointer = &heap[last];
+    last+=nbytes;
+    return pointer;
+  }
+  else
+  {
+    return  (void *) -1;
+  }
+}
+
+#include <errno.h>
+
+#undef errno
+extern int  errno;
+
+int
+_close (int   file)
+{
+  errno = EBADF;
+  
+  return -1;                    /* Always fails */
+}
+
+int
+_lseek (int   file,
+        int   offset,
+        int   whence)
+{
+  return  0;
+
+} 
+
+int
+_read (int   file,
+       char *ptr,
+       int   len)
+{
+  return  0;                            /* EOF */
+
+} 
+
+#include <sys/stat.h>
+
+int
+_fstat (int          file,
+        struct stat *st)
+{
+  st->st_mode = S_IFCHR;
+  return  0;
+
+}
+
+int
+_isatty (int   file)
+{
+  return  1;
+
+}  
